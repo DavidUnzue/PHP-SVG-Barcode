daf525ad2bb2fbbe78a8f7db574cd66c8f8934b4
Unzué Moriones, David <david.unzue@gim.biologisgroup.com>
2015-06-12 11:08:56 +0200

    Corrected EAN8 example file

--- EAN8/ean8example.php
+++ EAN8/ean8example.php
@@ -2,7 +2,8 @@
     ini_set('display_errors',1);
     error_reporting(E_ALL|E_STRICT);
     include 'EAN8.php';
+    $ean8 = new EAN8();
     $code = isset($_GET['code']) ? $_GET['code'] :'4400054'; 
     header("Content-type: image/svg+xml");
-    echo draw($code);
+    echo $ean8->draw($code);
 ?>
